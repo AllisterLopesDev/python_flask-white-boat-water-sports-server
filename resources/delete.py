758e181e6787af00f1689556ccd907d26f4c8dff
Allister Lopes <lopesallister.001@gmail.com>
2023-11-05 01:21:23 +0530

    added delete order api

--- resources/delete.py
+++ resources/delete.py
@@ -10,26 +10,60 @@ blue_print = Blueprint("delete_data", __name__)
 
 @blue_print.route('/get_order_details', methods=['GET'])
 def orderDetails():
-    ticket_no = request.args.get('serial_no')
-    if not ticket_no:
+    serial_no = request.args.get('serial_no')
+    if not serial_no:
         return jsonify({
                 'success': False,
-                'message': 'ticket_no required',
+                'message': 'serial_no required',
                 'status': 400}), 400
-    vehical_order_info = db.session.query(VehicalOrder.id, VehicalOrder.vehical_id, VehicalOrder.order_id, VehicalOrder.commission_amount, VehicalOrder.payment_status)\
-        .filter(VehicalOrder.id == (db.session.query(Order.id).filter(Order.serial_no == 'AWS1208').subquery())).first()
+    
+    order_exist = Order.query.filter_by(serial_no=serial_no).first()
 
-    result = {
-        'id': vehical_order_info[0],
-        'vehical_id': vehical_order_info[1],
-        'order_id': vehical_order_info[2],
-        'commission_amount': vehical_order_info[3],
-        'payment_status': vehical_order_info[4]
+    # if order_exist :
+    #     print(Order.id)
+    
+    if not order_exist:
+         return jsonify({
+                'success': False,
+                'message': 'record not found',
+                'status': 400}), 400
+
+    response = {
+        'id': order_exist.id,
+        'amount': order_exist.amount,
+        'serial_no': order_exist.serial_no,
+        'pax': order_exist.pax,
+        'created_at': order_exist.created_at,
+        'payment_method': order_exist.payment_method
     }
-    return jsonify(result)
+
+    return jsonify(response)
+
+
 
 # delete a order api
 @blue_print.route('/delete_order/<int:order_id>', methods=['DELETE'])
-def delete_order(order_id):
-    # Your delete logic here
+def delete_order(order_id): 
+
+    order_exist = Order.query.get(order_id)
+    if not order_exist:
+        return jsonify({
+        'success': False,
+        'message': 'order not found',
+        'status': 400
+        }), 400
+
+    vehical_order_exist = VehicalOrder.query.filter_by(order_id=order_id).first()
+    if vehical_order_exist:
+        db.session.delete(vehical_order_exist)
+        vehical_id = vehical_order_exist.vehical_id
+        vehical_exist = Vehical.query.filter_by(id=vehical_id).first()
+        
+        if vehical_exist:
+            db.session.delete(vehical_exist)
+    
+    db.session.delete(order_exist)
+
+    db.session.commit()
+
     return jsonify({'message': f'Deleted order with ID {order_id}'})
