a5daca0a32f8e0fa0961359f237fab1a4c05690b
Allister Lopes <lopesallister.001@gmail.com>
2023-11-04 23:18:19 +0530

    changes to booking response

--- resources/delete.py
+++ resources/delete.py
@@ -8,37 +8,20 @@ from models.vehical_order import VehicalOrder
 
 blue_print = Blueprint("delete_data", __name__)
 
-# delete a order api
-
-@blue_print.route("/delete_order/<string:serial_no>", methods=["GET"])
-def deleteOrderData(serial_no):
-
-    ticket_no = request.json.get(serial_no)
-    # initialize variable
-    order_id = 0
-
+@blue_print.route('/get_order_details', methods=['GET'])
+def orderDetails():
+    ticket_no = request.args.get('serial_no')
     if not ticket_no:
         return jsonify({
-            'success': False,
-            'message': 'ticket_no. is required',
-            'status': 400
-        }), 400
-    
-    order_record = Order.query.filter_by(serial_no=serial_no).first()
-    if order_record is not None:
-        order_id = order_record.id
-
+                'success': False,
+                'message': 'ticket_no required',
+                'status': 400}), 400
     
     
-    # try:
+    return
 
-
-
-        
-
-    # except Exception as e:
-    #     return jsonify({'error': 'An error occurred while generating the report.'}), 500
-
-    return jsonify({
-        'order_id': order_id,
-        }), 200
+# delete a order api
+@blue_print.route('/delete_order/<int:order_id>', methods=['DELETE'])
+def delete_order(order_id):
+    # Your delete logic here
+    return jsonify({'message': f'Deleted order with ID {order_id}'})
